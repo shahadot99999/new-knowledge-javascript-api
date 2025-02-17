cd218a142ab9c391543aa923199e513561f4763b
shahadot99999 <shahadot99999@gmail.com>
2025-02-17 11:43:18 +0600

    map file added

---
+++ map.js
@@ -0,0 +1,6 @@
+//map   The JavaScript Array.map() method allows you to create a new array 
+// by applying a function to each element of the original array in a concise, one-line manner. 
+
+const numbers = [1, 2, 3];
+const doubled = numbers.map(num => num * 2); // [2, 4, 6]
+console.log(doubled);
