29a246ca76bd815a9d648ddebb2b8d9657333995
shahadot99999 <shahadot99999@gmail.com>
2025-02-26 20:41:12 +0600

    url are added

--- js/task13.js
+++ js/task13.js
@@ -20,3 +20,11 @@ const people = [
   //   25: [{ name: "Alice", age: 25 }, { name: "Charlie", age: 25 }],
   //   30: [{ name: "Bob", age: 30 }, { name: "David", age: 30 }]
   // }
+
+
+  const urls = ["url1", "url2", "url3"];
+
+for (const url of urls) {
+  const response = await fetch(url);
+  console.log(await response.json());
+}
